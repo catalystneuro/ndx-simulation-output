d3f26d4747fac5f20a94bc5ba088ff22c7dd8f9f
Ryan Ly <rly@lbl.gov>
2019-10-15 23:34:14 -0700

    Remove help attribute, clean up docstrings

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -7,8 +7,8 @@ from pynwb.spec import NWBNamespaceBuilder, export_spec, NWBGroupSpec
 
 def main():
     # these arguments were auto-generated from your cookiecutter inputs
-    ns_builder = NWBNamespaceBuilder(doc='Holds structures for recording data from multiple compartments of multiple '
-                                         'neurons in a single TimeSeries',
+    ns_builder = NWBNamespaceBuilder(doc='Data types for recording data from multiple compartments of multiple '
+                                         'neurons in a single TimeSeries.',
                                      name='ndx-simulation-output',
                                      version='0.2.5',
                                      author='Ben Dichter',
@@ -18,32 +18,52 @@ def main():
     for ndtype in types_to_include:
         ns_builder.include_type(ndtype, namespace='core')
 
-    Compartments = NWBGroupSpec(default_name='compartments', neurodata_type_def='Compartments',
+    Compartments = NWBGroupSpec(default_name='compartments',
+                                neurodata_type_def='Compartments',
                                 neurodata_type_inc='DynamicTable',
-                                doc='table that holds information about what places are being recorded')
-    Compartments.add_dataset(name='number', neurodata_type_inc='VectorData', dtype='int',
-                             doc='cell compartment ids corresponding to a each column in the data')
-    Compartments.add_dataset(name='number_index', neurodata_type_inc='VectorIndex',
-                             doc='maps cell to compartments', quantity='?')
-    Compartments.add_dataset(name='position', neurodata_type_inc='VectorData', dtype='float', quantity='?',
-                             doc='position of recording within a compartment. 0 is close to soma, 1 is other end')
-    Compartments.add_dataset(name='position_index', neurodata_type_inc='VectorIndex',
-                             doc='indexes position', quantity='?')
-    Compartments.add_dataset(name='label', neurodata_type_inc='VectorData', doc='labels for compartments',
-                             dtype='text', quantity='?')
-    Compartments.add_dataset(name='label_index', neurodata_type_inc='VectorIndex', doc='indexes label', quantity='?')
+                                doc='Table that holds information about what places are being recorded.')
+    Compartments.add_dataset(name='number',
+                             neurodata_type_inc='VectorData',
+                             dtype='int',
+                             doc='Cell compartment ids corresponding to a each column in the data.')
+    Compartments.add_dataset(name='number_index',
+                             neurodata_type_inc='VectorIndex',
+                             doc='Index that maps cell to compartments.',
+                             quantity='?')
+    Compartments.add_dataset(name='position',
+                             neurodata_type_inc='VectorData',
+                             dtype='float',
+                             quantity='?',
+                             doc='Position of recording within a compartment. 0 is close to soma, 1 is other end.')
+    Compartments.add_dataset(name='position_index',
+                             neurodata_type_inc='VectorIndex',
+                             doc='Index for position.',
+                             quantity='?')
+    Compartments.add_dataset(name='label',
+                             neurodata_type_inc='VectorData',
+                             doc='Labels for compartments.',
+                             dtype='text',
+                             quantity='?')
+    Compartments.add_dataset(name='label_index',
+                             neurodata_type_inc='VectorIndex',
+                             doc='indexes label',
+                             quantity='?')
 
-    CompartmentsSeries = NWBGroupSpec(neurodata_type_def='CompartmentSeries', neurodata_type_inc='TimeSeries',
+    CompartmentsSeries = NWBGroupSpec(neurodata_type_def='CompartmentSeries',
+                                      neurodata_type_inc='TimeSeries',
                                       doc='Stores continuous data from cell compartments')
-    CompartmentsSeries.add_link(name='compartments', target_type='Compartments', quantity='?',
-                                doc='meta-data about compartments in this CompartmentSeries')
+    CompartmentsSeries.add_link(name='compartments',
+                                target_type='Compartments',
+                                quantity='?',
+                                doc='Metadata about compartments in this CompartmentSeries.')
 
-    SimulationMetaData = NWBGroupSpec(name='simulation', neurodata_type_def='SimulationMetaData',
-                                      neurodata_type_inc='LabMetaData', doc='group that holds metadata for simulation')
-    SimulationMetaData.add_group(name='compartments', neurodata_type_inc='Compartments',
-                                 doc='table that holds information about what places are being recorded')
-    SimulationMetaData.add_attribute(name='help', dtype='text', doc='help',
-                                     value='container for simulation meta-data that goes in /general')
+    SimulationMetaData = NWBGroupSpec(name='simulation',
+                                      neurodata_type_def='SimulationMetaData',
+                                      neurodata_type_inc='LabMetaData',
+                                      doc='Group that holds metadata for simulations.')
+    SimulationMetaData.add_group(name='compartments',
+                                 neurodata_type_inc='Compartments',
+                                 doc='Table that holds information about what places are being recorded.')
 
     new_data_types = [Compartments, CompartmentsSeries, SimulationMetaData]
 
